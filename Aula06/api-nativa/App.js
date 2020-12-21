c7fb8103c0c5de3927a333fc8004a944b3292819
FIC <FIC@info.local>
2020-12-21 12:30:11 -0200

    aula04 implementada na aula03-nyous

--- Aula06/api-nativa/App.js
+++ Aula06/api-nativa/App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Contatos from './Pages/Contatos';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+      <Tab.Screen name="Contatos" component={Contatos}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- Aula06/api-nativa/Pages/Contatos/index.js
+++ Aula06/api-nativa/Pages/Contatos/index.js
@@ -1,21 +1,66 @@
-import React from 'react';
-import {View, Text, } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View, Text, StatusBar, FlatList} from 'react-native';
+import * as Contacts from 'expo-contacts';
 
     
-    const style = StyleSheet.create({
+    const styles = StyleSheet.create({
         container : {
             flex : 1,
-            margintop : StatusBar.currentHeight || 0
+            marginTop : StatusBar.currentHeight || 0
         }
 
     })
 
+    const ItemContato = ({nome}) => {
+       return(
+        <View>
+        <Text>{nome}</Text>
+        </View>
+       )
+    }
     
     const Contatos = () => {
+
+        const [contatos, setContatos] = useState([]);
+
+        // Ciclo de vida, assim que aparecer a página passa pelo useEffect
+        useEffect(() => {
+            (async () => {
+
+              // Requisita a permissão do usuário para obter os contatos
+              const { status } = await Contacts.requestPermissionsAsync();
+              // Permissão concedida pelo usuário
+              if (status === 'granted') {
+               // Pega todos os contatos   
+                const { data } = await Contacts.getContactsAsync({
+                  fields: [Contacts.Fields.Emails],
+                });
+        
+                //Vefifica se existe contatos cadastrados
+                if (data.length > 0) {
+                    console.log(data);
+                  setContatos(data);
+                }
+              }
+            })();
+          }, []);
+
+          const renderItem = ({item}) => {
+                return (
+                <ItemContato nome = {item.name} />
+                )
+                    
+          }
+
         return(
             <View style = {styles.container}>
                 <Text>Contatos</Text>
-            </View>
+                <FlatList
+                    data =          {contatos}
+                    keyExtractor =  {item => item.id}
+                    renderItem =    {renderItem}
+                    />
+            </View> 
         )
     }
 
